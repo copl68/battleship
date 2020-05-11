640fb1fae2827da61b8a5f6b5acd0b7517c33802
copl68 <54590893+copl68@users.noreply.github.com>
2020-05-11 14:56:30 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,16 +1,16 @@
 #include "sockets.h"
 
 int main(int argc, char* argv[]){
-	int sockfd;
-	if(argc == 2){
-		sockfd = CreateServer(argv);
-	}
-	else if(argc == 3){
-		sockfd = CreateClient(argv);
-	}
-	else{
-		fprintf(stderr, "Invalid Use...\nServer use: ./final <port>\nClient use: ./final <port> <server_name>\n");
-		exit(0);
-	}	
+        int sockfd;
+        if(argc == 2){
+                sockfd = CreateServer(argv);
+        }
+        else if(argc == 3){
+                sockfd = CreateClient(argv);
+        }
+        else{
+                fprintf(stderr, "\nInvalid Use...\nServer use: ./final <port>\nClient use: ./final <port> <server_name>\n\n");
+                exit(0);
+        }
 
 }
